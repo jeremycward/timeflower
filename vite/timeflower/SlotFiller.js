4f6e27bdb07569e17d4ffd07bf392e7ef72abd9f
jeremycward <jeremy_c_ward@yahoo.co.uk>
2024-12-30 10:40:08 +0000

    small changes before switch to wc

---
+++ vite/timeflower/SlotFiller.js
@@ -0,0 +1,23 @@
+
+export class SlotFillerJobSpec{
+    constructor(headerSlots,itemSlots){
+        super.constructor()
+        this.headerSlots = headerSlots
+        this.itemSlots = itemSlots
+    }
+
+
+}
+
+export class SlotFiller{
+    constructor(fillerJobSpec){
+        super.constructor()
+        this.fillerJobSpec = fillerJobSpec
+    }
+
+    fillSlots(){
+
+    }
+
+
+}
